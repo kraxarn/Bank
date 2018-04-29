16bc6c3b19f606bd69302a7cef9afc22d7c16d1e
kraxarn <emil202821@hotmail.com>
2018-04-29 15:52:00 +0200

    Basic settings

---
+++ Bank/Bank/Views/SettingsPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bank.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SettingsPage : ContentPage
+	{
+		public SettingsPage() 
+			=> InitializeComponent();
+
+		private void DarkMode_OnChanged(object sender, ToggledEventArgs e)
+		{
+			if (!e.Value)
+				return;
+
+			Application.Current.MainPage.DisplayAlert("Sorry", "Not implemented yet", "OK");
+			CellDarkMode.On = false;
+		}
+
+		private async void ButtonProfile_OnClicked(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new ProfilePage());
+		}
+	}
+}
