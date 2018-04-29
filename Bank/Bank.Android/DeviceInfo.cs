c00401860691fc8e6263c90d6ea593ab46c8b700
kraxarn <emil202821@hotmail.com>
2018-04-29 17:03:06 +0200

    Implement DeviceInfo for Android

---
+++ Bank/Bank.Android/DeviceInfo.cs
@@ -0,0 +1,12 @@
+﻿using Android.OS;
+using Bank.Droid;
+
+[assembly: Xamarin.Forms.Dependency(typeof(DeviceInfo))]
+namespace Bank.Droid
+{
+	public class DeviceInfo : IDeviceInfo
+	{
+		public string DeviceName => Build.Model;
+		public string OSVersion  => $"Android {Build.VERSION.Sdk}";
+	}
+}
