c28b0cd47934300e6f96ba7e80ad0906c32f0c80
kraxarn <emil202821@hotmail.com>
2018-04-29 17:09:09 +0200

    Implement DeviceInfo for UWP

---
+++ Bank/Bank.UWP/DeviceInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bank.UWP;
+
+[assembly: Xamarin.Forms.Dependency(typeof(DeviceInfo))]
+namespace Bank.UWP
+{
+	public class DeviceInfo : IDeviceInfo
+	{
+		public string DeviceName => Environment.MachineName;
+		public string OSVersion  => $"{Environment.OSVersion}";
+	}
+}
