519e76f5463871f45b076ab7105be887083f2070
kraxarn <emil202821@hotmail.com>
2018-05-01 11:40:23 +0200

    Add copyright information

--- Bank/Bank.UWP/Properties/AssemblyInfo.cs
+++ Bank/Bank.UWP/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Bank.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © kraxarn 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
