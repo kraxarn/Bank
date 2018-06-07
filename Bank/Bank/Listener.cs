49d1e45ad744aca2e49d2aea87b1210004a76da2
kraxarn <emil202821@hotmail.com>
2018-06-07 17:38:10 +0200

    Add Listener class

---
+++ Bank/Bank/Listener.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using Xamarin.Forms;
+
+namespace Bank
+{
+	/*
+	 * A listener is similar to the server,
+	 * but it only listens for data and triggers
+	 * events based on the response.
+	 */
+
+	/*
+	 * OnJoin
+	 * OnAdd
+	 * OnRemove
+	 * OnSet
+	 * OnNew
+	 */
+
+	public class Listener
+	{
+
+		#region Events
+
+		public delegate void JoinEvent(User joinedUser);
+		public delegate void MoneyEvent(User changedUser); // Or just address and money
+
+		/// <summary>
+		/// A player has joined (or already joined)
+		/// </summary>
+		public event JoinEvent  PlayerJoined;
+
+		/// <summary>
+		/// Money has changed for a user. 
+		/// This is triggered twice if money is transfered.
+		/// </summary>
+		public event MoneyEvent MoneyChanged;
+
+		#endregion
+
+		private readonly TcpListener server;
+		private readonly List<User>  users;
+		
+		public bool Running { get; private set; }
+
+		public Listener()
+		{
+			var ip = IPAddress.Parse("127.0.0.1");
+			// Assume port
+			server = new TcpListener(ip, 13000);
+			Running = false;
+		}
+
+		private void InvokeNewPlayer(User user) 
+			=> PlayerJoined?.Invoke(user);
+
+		private void InvokeNewPlayer(string name, int avatar, string address)
+			=> InvokeNewPlayer(new User(name, avatar, address));
+
+		private void InvokeMoneyChange(User user)
+			=> MoneyChanged?.Invoke(user);
+
+		public bool Start(out string error)
+		{
+			try
+			{
+				server.Start();
+				var thread = new Thread(Thread);
+				thread.Start();
+			}
+			catch (SocketException e)
+			{
+				server?.Stop();
+				error = e.Message;
+				return false;
+			}
+
+			Running = true;
+			error = null;
+			return true;
+		}
+
+		private void Thread()
+		{
+			// Similar to Server.ServerThread
+			var bytes = new byte[256];
+
+			while (Running)
+			{
+				TcpClient client = null;
+
+				try
+				{
+					client = server.AcceptTcpClient();
+				}
+				catch (SocketException)
+				{
+					client?.Close();
+					continue;
+				}
+				catch (TargetInvocationException)
+				{
+					client?.Close();
+					continue;
+				}
+
+				var ok = Encoding.ASCII.GetBytes("OK");
+				var stream = client.GetStream();
+				int i;
+
+				while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+				{
+					var data = Encoding.ASCII.GetString(bytes, 0, i);
+
+					if (data == "STOP")
+					{
+						stream.Write(ok, 0, ok.Length);
+						Running = false;
+						break;
+					}
+
+					var dat = data.Split(',');
+
+					if (dat[0] == "JOIN")
+					{
+						var u = new User(dat[1], int.Parse(dat[2]), ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString());
+						InvokeNewPlayer(u);
+						users.Add(u);
+					}
+					else if (dat[0] == "ADD")
+					{
+						var user = users.Single(u => u.Address == dat[1]);
+
+						if (user != null && uint.TryParse(dat[2], out var amount))
+						{
+							user.Money += amount;
+							InvokeMoneyChange(user);
+						}
+						else
+							DisplayAlert("Failed to add money", "The specified user could not be found");
+					}
+
+					stream.Write(ok, 0, ok.Length);
+				}
+
+				client.Close();
+			}
+			
+			server.Stop();
+		}
+
+		private void DisplayAlert(string title, string message, string button = "Dismiss")
+			=> Application.Current.MainPage.DisplayAlert(title, message, button);
+	}
+}
