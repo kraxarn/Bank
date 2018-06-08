951c1c816f5572b76207aba2135f5592099244dd
kraxarn <emil202821@hotmail.com>
2018-06-08 16:20:22 +0200

    Add waiting page

---
+++ Bank/Bank/Views/WaitPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bank.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class WaitPage : ContentPage
+	{
+		public WaitPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
