d8e0ef6a2d1d7324e39509e177ebc0f2feba3775
kraxarn <emil202821@hotmail.com>
2018-06-09 16:27:38 +0200

    Minor cleanup

--- Bank/Bank/Views/WaitPage.xaml.cs
+++ Bank/Bank/Views/WaitPage.xaml.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Bank.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class WaitPage : ContentPage
+	public partial class WaitPage
 	{
 		private readonly Client client;
 
