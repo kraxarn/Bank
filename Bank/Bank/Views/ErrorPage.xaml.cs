1fb232fc2e45589ea2bb376134a62de73f4bcecf
kraxarn <emil202821@hotmail.com>
2018-06-17 11:08:38 +0200

    Add error page for details

---
+++ Bank/Bank/Views/ErrorPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bank.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ErrorPage : ContentPage
+	{
+		public ErrorPage(Exception e)
+		{
+			InitializeComponent();
+
+			LabelTitle.Text = e.GetType().FullName;
+			LabelDetails.Text = e.StackTrace;
+		}
+	}
+}
