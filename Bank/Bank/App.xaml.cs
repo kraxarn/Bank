767ac8d0e601801c5b597c603a2afbe3d310c556
kraxarn <emil202821@hotmail.com>
2018-07-01 13:51:39 +0200

    Add events for handling sleep/resume

--- Bank/Bank/App.xaml.cs
+++ Bank/Bank/App.xaml.cs
@@ -4,6 +4,12 @@ namespace Bank
 {
 	public partial class App
 	{
+		public delegate void SleepEvent();
+		public delegate void ResumeEvent();
+
+		public static event SleepEvent  OnAppSleep;
+		public static event ResumeEvent OnAppResume;
+
 		public App()
 		{
 			InitializeComponent();
@@ -19,11 +25,13 @@ namespace Bank
 		protected override void OnSleep ()
 		{
 			// Handle when your app sleeps
+			OnAppSleep?.Invoke();
 		}
 
 		protected override void OnResume ()
 		{
 			// Handle when your app resumes
+			OnAppResume?.Invoke();
 		}
 	}
 }
