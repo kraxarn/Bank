a9c03fa361e9c9cedb1832001c6185cadef8db11
kraxarn <emil202821@hotmail.com>
2018-08-01 23:46:35 +0200

    Fix android style build error

--- Bank/Bank.Android/Resources/Resource.designer.cs
+++ Bank/Bank.Android/Resources/Resource.designer.cs
@@ -3872,8 +3872,8 @@ namespace Bank.Droid
 			// aapt resource value: 0x7f0b0170
 			public const int Animation_Design_BottomSheetDialog = 2131427696;
 			
-			// aapt resource value: 0x7f0b018d
-			public const int AppCompatDialogStyle = 2131427725;
+			// aapt resource value: 0x7f0b018f
+			public const int AppCompatDialogStyle = 2131427727;
 			
 			// aapt resource value: 0x7f0b00b2
 			public const int Base_AlertDialog_AppCompat = 2131427506;
@@ -4354,6 +4354,12 @@ namespace Bank.Droid
 			
 			// aapt resource value: 0x7f0b018a
 			public const int MainTheme_Base = 2131427722;
+			
+			// aapt resource value: 0x7f0b018d
+			public const int MainThemeBlack = 2131427725;
+			
+			// aapt resource value: 0x7f0b018e
+			public const int MainThemeBlack_Base = 2131427726;
 			
 			// aapt resource value: 0x7f0b018b
 			public const int MainThemeDark = 2131427723;
